e873ad5b03ba7b73beac34612bbb22dc09e27497
ms666 <1550324080@qq.com>
2021-08-27 17:19:14 +0800

    2021/8/27 SpringBoot Admin安全配置,解决Admin Server被监控问题

--- cloud-yblog-monitor/cloud-yblog-monitor8901/src/main/java/com/boot/MonitorApplication8901.java
+++ cloud-yblog-monitor/cloud-yblog-monitor8901/src/main/java/com/boot/MonitorApplication8901.java
@@ -3,18 +3,16 @@ package com.boot;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 游政杰
  * 2021/8/27 12:12
  */
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAdminServer //说明这个SpringBoot应用是SpringBoot admin server主程序
-public class MonitorApplication {
+public class MonitorApplication8901 {
 
   public static void main(String[] args) {
-      SpringApplication.run(MonitorApplication.class,args);
+      SpringApplication.run(MonitorApplication8901.class,args);
   }
 }
